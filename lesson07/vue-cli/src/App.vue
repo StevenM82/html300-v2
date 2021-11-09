d6699d02e4ad3cc025c99c5e83d3625fe87cf27f
Gitting better <StevenM82@users.noreply.github.com>
2021-11-09 12:55:19 -0800

    moving things

--- lesson07/vue-cli/src/App.vue
+++ lesson07/vue-cli/src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <button>Click me</button>
-    <button :disabled="isActive">Submit</button>
-  </div> 
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -11,12 +24,7 @@ export default {
   name: 'app',
   data () {
     return {
-      isActive: true, 
-    }
-  },
-  methods: {
-    function () {
-      $('[data-toggle="tooltip"]').tooltip()
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
@@ -48,13 +56,5 @@ li {
 
 a {
   color: #42b983;
-}
-.demo {
-  height: 100px;
-  width: 100px;
-  background-color: green;
-}
-.red {
-  background-color: red;
 }
 </style>
