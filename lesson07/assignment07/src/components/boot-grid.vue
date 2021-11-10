fe93739b0a6471c03983c6ed5d0ce93d82f46ded
Gitting better <StevenM82@users.noreply.github.com>
2021-11-09 22:50:07 -0800

    updated the images.vue

--- lesson07/assignment07/src/components/boot-grid.vue
+++ lesson07/assignment07/src/components/boot-grid.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="container">
-    <div class="row text-center">
-      <h1 class="col-12 display-3">Wanderlust a la Carte</h1>
-      <h2 class="col-12 display-4">Not lost, but finding</h2>
-    </div>
+  <div id="">
+    <div class="container">
+      <div class="row text-center">
+        <h1 class="col-12 display-3">Wanderlust a la Carte</h1>
+        <h2 class="col-12 display-4">Not lost, but finding</h2>
+      </div>
 
-    <!-- the outer two columns adjust automatically, regardless of screen size. the image is made responsive via img-fluid -->
-    <div class="row mt-1">
-      <p class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rerum ipsam praesentium dolor aliquid? Dolorem quas, vitae at odit eaque, eius excepturi voluptate ex est pariatur iure, odio saepe minima.</p>
-      <img class="col-sm-3 col-md-5 col-lg-2 img-thumbnail img-fluid" src="images/The-beginning.jpg">
-      <p class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nulla tenetur dolore, ea incidunt aliquid reprehenderit natus officiis, illo quasi tempore ad dolorum eius repellendus obcaecati ullam odio voluptatibus. Impedit!</p>
-    </div>
+      <!-- the outer two columns adjust automatically, regardless of screen size. the image is made responsive via img-fluid -->
+      <div class="row mt-1">
+        <p class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rerum ipsam praesentium dolor aliquid? Dolorem quas, vitae at odit eaque, eius excepturi voluptate ex est pariatur iure, odio saepe minima.</p>
+        <img class="col-sm-3 col-md-5 col-lg-2 img-thumbnail img-fluid" src="images/The-beginning.jpg">
+        <p class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nulla tenetur dolore, ea incidunt aliquid reprehenderit natus officiis, illo quasi tempore ad dolorum eius repellendus obcaecati ullam odio voluptatibus. Impedit!</p>
+      </div>
 
-    <!-- make the three columns equal on screens over 768px wide -->
-    <div class="row mt-2 mb-2">
-      <p class="col-sm-3 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, possimus tempore cupiditate fugit quasi quis maxime quod cumque corrupti, ipsum dolorem? Dolorum, minima! Cum animi, veniam reiciendis delectus repellendus sapiente.</p>
-      <img class="col-sm-6 col-md-4 img-thumbnail" src="images/floored.jpg">
-      <p class="col-sm-3 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsam, aperiam sit facere facilis laudantium harum est ab consequatur! Tenetur, minus. Soluta quam praesentium iste distinctio eum maxime quas est?</p>
-    </div>
+      <!-- make the three columns equal on screens over 768px wide -->
+      <div class="row mt-2 mb-2">
+        <p class="col-sm-3 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, possimus tempore cupiditate fugit quasi quis maxime quod cumque corrupti, ipsum dolorem? Dolorum, minima! Cum animi, veniam reiciendis delectus repellendus sapiente.</p>
+        <img class="col-sm-6 col-md-4 img-thumbnail" src="images/floored.jpg">
+        <p class="col-sm-3 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsam, aperiam sit facere facilis laudantium harum est ab consequatur! Tenetur, minus. Soluta quam praesentium iste distinctio eum maxime quas est?</p>
+      </div>
 
-    <!-- this row does not show up on screens smaller than 768px  -->
-    <div class="row">
-      <p class="col-md-6 offset-md-3 text-center d-none d-md-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptas doloremque neque, iusto, unde omnis, a quasi ratione numquam eveniet asperiores quae architecto nostrum in velit dolores aperiam accusantium quis?</p>
+      <!-- this row does not show up on screens smaller than 768px  -->
+      <div class="row">
+        <p class="col-md-6 offset-md-3 text-center d-none d-md-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptas doloremque neque, iusto, unde omnis, a quasi ratione numquam eveniet asperiores quae architecto nostrum in velit dolores aperiam accusantium quis?</p>
+      </div>
     </div>
   </div>
 </template>
@@ -29,9 +31,9 @@
 <script>
   export default {
     data() {
-        return {
-            
-        }
+      return {
+          
+      }
     }
   }
 </script>
@@ -57,24 +59,3 @@
   }
   /* Set a black box-shadow around the card on hover */
 </style>
-
-
-<!-- This moves the heading up and down -->
-    <!-- https://codepen.io/ZachSaucier/pen/RwVdEJO -->
-    <!-- <main class="main" aria-label="You expect this pen to do one thing but it changed into something else. — Zach Saucier">
-      <blockquote cite="" aria-hidden>
-
-        <p class="colored-text">
-          <span>but finding</span>
-        </p>
-
-        <div class="cut-text-container">
-          <h1 class="cut-text"><span>Wanderlust</span></h1>
-          <h2 class="cut-text"><span>a la Carte</span></h2>
-        </div>
-
-        <p class="colored-text second">
-          <span>Not lost</span>
-        </p>
-      </blockquote>
-    </main> -->
--- lesson07/assignment07/src/components/images.vue
+++ lesson07/assignment07/src/components/images.vue
@@ -1,18 +1,20 @@
 <template>
-        <ul class="section-images">
+    <div id="">
+        <ul class="section-images text-decoration-none p-0">
             <li><img class="img-fluid" data-toggle="tooltip" data-placement="left" title="As delicious as it looks!"
-                src="images/camping-wings-veggies.jpg" alt="chicken wings and skewered veggies
+                src="./assets/camping-wings-veggies.jpg" alt="chicken wings and skewered veggies
                 cooking over a fire"></li>
-            <li><img class="img-fluid" src="assets/the-market.jpg"></li>
-            <li><img class="img-fluid" src="assets/tropical-forest.jpg"></li>
-            <li><img class="img-fluid" src="assets/glacial-lake.jpg"></li>
-            <li><img class="img-fluid" src="assets/festival-2019.jpg"></li>
-            <li><img class="img-fluid" src="assets/los-muertos.jpg"></li>
-            <li><img class="img-fluid" src="assets/original-interior.jpg"></li>
-            <li><img class="img-fluid" src="assets/original-interior-2.jpg"></li>
-            <li><img class="img-fluid" src="assets/floor-removed.jpg"></li>
-            <li><img class="img-fluid" src="assets/sliding-door-prep.jpg"></li>
+            <li><img class="img-fluid" src="./assets/the-market.jpg"></li>
+            <li><img class="img-fluid" src="./assets/tropical-forest.jpg"></li>
+            <li><img class="img-fluid" src="./assets/glacial-lake.jpg"></li>
+            <li><img class="img-fluid" src="./assets/festival-2019.jpg"></li>
+            <li><img class="img-fluid" src="./assets/los-muertos.jpg"></li>
+            <li><img class="img-fluid" src="./assets/original-interior.jpg"></li>
+            <li><img class="img-fluid" src="./assets/original-interior-2.jpg"></li>
+            <li><img class="img-fluid" src="./assets/floor-removed.jpg"></li>
+            <li><img class="img-fluid" src="./assets/sliding-door-prep.jpg"></li>
         </ul>
+    </div>
 </template>
 
 <script>
@@ -26,38 +28,45 @@
 </script>
 
 <style>
-    /* Set the section element to be the outer grid container for the body of the page
-    This is used on the images.html page only */
-    .section-images {
-    @include grid-main;
-    grid-area: 2 / 2 / 10 / 12;
-    /* This gives grid dimensions for the nth-child of the images list */
-    li:nth-child(1) {
-        grid-column: span 2;
-        grid-row: span 1;
-    }
-    li:nth-child(2) {
-        grid-column: span 1;
-        grid-row: span 5;
-    }
-    li:nth-child(4) {
-        grid-row: span 2;
-    }
-    li:nth-child(6) {
-        grid-column: span 3;
-        grid-row: span 2;
-    }
-    li:nth-child(7) {
-        grid-column: span 1;
-        grid-row: span 1;
-    }
-    li:nth-child(8) {
-        grid-column: span 2;
-    }
-    li:nth-child(10) {
-        grid-column: span 2;
-        grid-row: span 2;
-    }
-    }
+
+/* Set the section element to be the outer grid container for the body of the page
+This is used on the images.html page only */
+
+.section-images {
+  --auto-grid-min-size: 16rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(var(--auto-grid-min-size), 1fr));
+  grid-area: 2 / 2 / 10 / 12;
+  gap: 0.5rem;
+  list-style: none;
+  
+  /* This gives grid dimensions for the nth-child of the images list */
+  li:nth-child(1) {
+      grid-column: span 2;
+      grid-row: span 1;
+  }
+  li:nth-child(2) {
+      grid-column: span 1;
+      grid-row: span 5;
+  }
+  li:nth-child(4) {
+      grid-row: span 2;
+  }
+  li:nth-child(6) {
+      grid-column: span 3;
+      grid-row: span 2;
+  }
+  li:nth-child(7) {
+      grid-column: span 1;
+      grid-row: span 1;
+  }
+  li:nth-child(8) {
+      grid-column: span 2;
+  }
+  li:nth-child(10) {
+      grid-column: span 2;
+      grid-row: span 2;
+  }
+}
 </style>
 
