786539b4cb71c97252b432d7e38efe3a85ae564c
Gitting better <StevenM82@users.noreply.github.com>
2021-11-09 17:23:54 -0800

    removed html <head> information and tags

--- lesson07/assignment07/src/components/accordian.vue
+++ lesson07/assignment07/src/components/accordian.vue
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <link href='http://fonts.googleapis.com/css?family=Nothing+You+Could+Do' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/style.css">
-    <title>Wanderlust a la Carte</title>
-  </head>
-
-<body>
+<template>
   <div class="grid-container">
     <main class="main">
       <img class="header-img" src="images/lopez-island.jpg"
@@ -106,8 +94,7 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
   <script src="index.js"></script>
-</body>
-</html>
+</template>
 
 
 <script>
