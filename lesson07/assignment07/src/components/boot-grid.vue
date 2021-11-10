dd4c16a00f37563f6367a990826d47d53ee75e01
Gitting better <StevenM82@users.noreply.github.com>
2021-11-09 23:02:36 -0800

    changed the paths for the images. both the boot-grid.vue and images.vue need to have a main id assigned

--- lesson07/assignment07/src/components/boot-grid.vue
+++ lesson07/assignment07/src/components/boot-grid.vue
@@ -9,14 +9,14 @@
       <!-- the outer two columns adjust automatically, regardless of screen size. the image is made responsive via img-fluid -->
       <div class="row mt-1">
         <p class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rerum ipsam praesentium dolor aliquid? Dolorem quas, vitae at odit eaque, eius excepturi voluptate ex est pariatur iure, odio saepe minima.</p>
-        <img class="col-sm-3 col-md-5 col-lg-2 img-thumbnail img-fluid" src="images/The-beginning.jpg">
+        <img class="col-sm-3 col-md-5 col-lg-2 img-thumbnail img-fluid" src="./assets/The-beginning.jpg">
         <p class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nulla tenetur dolore, ea incidunt aliquid reprehenderit natus officiis, illo quasi tempore ad dolorum eius repellendus obcaecati ullam odio voluptatibus. Impedit!</p>
       </div>
 
       <!-- make the three columns equal on screens over 768px wide -->
       <div class="row mt-2 mb-2">
         <p class="col-sm-3 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, possimus tempore cupiditate fugit quasi quis maxime quod cumque corrupti, ipsum dolorem? Dolorum, minima! Cum animi, veniam reiciendis delectus repellendus sapiente.</p>
-        <img class="col-sm-6 col-md-4 img-thumbnail" src="images/floored.jpg">
+        <img class="col-sm-6 col-md-4 img-thumbnail" src="./assets/floored.jpg">
         <p class="col-sm-3 col-md-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam ipsam, aperiam sit facere facilis laudantium harum est ab consequatur! Tenetur, minus. Soluta quam praesentium iste distinctio eum maxime quas est?</p>
       </div>
 
@@ -49,12 +49,6 @@
     /* make the articles appear more centered on smaller screens */
     @include media-query("mobile") {
       grid-area: 2 / 2 / 10 / 12;
-    }
-
-    /* allow the grid to place the as many articles that span 3 columns on one row as
-    fit on the screen */
-    .article {
-      grid-column: span 3;
     }
   }
   /* Set a black box-shadow around the card on hover */
