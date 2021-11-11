7fdc29b74925c338fc5abca7e19ee35ef01cffa0
Gitting better <StevenM82@users.noreply.github.com>
2021-11-10 19:31:31 -0800

    b

--- lesson07/assignment07/src/components/images.vue
+++ lesson07/assignment07/src/components/images.vue
@@ -1,19 +1,19 @@
 <template>
     <div id="images">
-        //<ul class="section-images text-decoration-none p-0">
-            //<li><img class="img-fluid" data-toggle="tooltip" data-placement="left" title="As delicious as it looks!"
-                //src="./assets/camping-wings-veggies.jpg" alt="chicken wings and skewered veggies
-                //cooking over a fire"></li>
-            //<li><img class="img-fluid" src="./assets/the-market.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/tropical-forest.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/glacial-lake.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/festival-2019.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/los-muertos.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/original-interior.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/original-interior-2.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/floor-removed.jpg"></li>
-            //<li><img class="img-fluid" src="./assets/sliding-door-prep.jpg"></li>
-        //</ul>
+        <!-- <ul class="section-images text-decoration-none p-0">
+            <li><img class="img-fluid" data-toggle="tooltip" data-placement="left" title="As delicious as it looks!"
+                src="./assets/camping-wings-veggies.jpg" alt="chicken wings and skewered veggies
+                cooking over a fire"></li>
+            <li><img class="img-fluid" src="./assets/the-market.jpg"></li>
+            <li><img class="img-fluid" src="./assets/tropical-forest.jpg"></li>
+            <li><img class="img-fluid" src="./assets/glacial-lake.jpg"></li>
+            <li><img class="img-fluid" src="./assets/festival-2019.jpg"></li>
+            <li><img class="img-fluid" src="./assets/los-muertos.jpg"></li>
+            <li><img class="img-fluid" src="./assets/original-interior.jpg"></li>
+            <li><img class="img-fluid" src="./assets/original-interior-2.jpg"></li>
+            <li><img class="img-fluid" src="./assets/floor-removed.jpg"></li>
+            <li><img class="img-fluid" src="./assets/sliding-door-prep.jpg"></li>
+        </ul> -->
         <ul id="listOfImages">
             <li v-for="photo in imagesNow>
                 {{photo.image}}
@@ -33,8 +33,8 @@
 
 <style lang="scss">
 
-/* Set the section element to be the outer grid container for the body of the page
-This is used on the images.html page only */
+<!-- Set the section element to be the outer grid container for the body of the page
+This is used on the images.html page only -->
 
 .section-images {
   --auto-grid-min-size: 16rem;
