79a9a53482c55ae304d7997a29efdc19fddf7c9d
Gitting better <StevenM82@users.noreply.github.com>
2021-11-17 22:44:25 -0800

    git status  typo

--- lesson07/assignment07/src/App.vue
+++ lesson07/assignment07/src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-    <div class="grid-container">
-      <main class="main">
-        <v-img class="header-img" src="src/assets/lopez-island.jpg"
-          alt="A view looking out toward the water from a tree filled and rocky island"></v-img>
-        <div class="flex-heading">
-          <h1>Wanderlust a la Carte</h1>
-          <h2>Not lost, but finding</h2>
-        </div>
-      </main>
-      <nav class="nav">
-        <ul class="nav-list">
-          <!-- <router-link> acts like an <a> with the correct href -->
-          <li><router-link to="/">The Beginning</router-link></li>
-          <li><router-link to="/images">Images</router-link></li>
-          <li><router-link to="/boot-grid">Boot-grid</router-link></li>
-          <li><router-link to="/accordian">Accordian</router-link></li>
-          <li>
-              <span data-toggle="tooltip" title="Not working yet">
-                  <a class="nav-link disabled" href="#">More and more</a>
-              </span>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <div class="grid-container">
+    <main class="main">
+      <img class="header-img" src="src/assets/lopez-island.jpg"
+        alt="A view looking out toward the water from a tree filled and rocky island">
+      <div class="flex-heading">
+        <h1>Wanderlust a la Carte</h1>
+        <h2>Not lost, but finding</h2>
+      </div>
+    <nav class="nav">
+      <ul class="nav-list">
+        <!-- <router-link> acts like an <a> with the correct href -->
+        <li><router-link to="/">The Beginning</router-link></li>
+        <li><router-link to="/images">Images</router-link></li>
+        <li><router-link to="/boot-grid">Boot-grid</router-link></li>
+        <li><router-link to="/accordian">Accordian</router-link></li>
+        <li>
+            <span data-toggle="tooltip" title="Not working yet">
+                <a class="nav-link disabled" href="#">More and more</a>
+            </span>
+        </li>
+      </ul>
+    </nav>
+    </main>
+    <router-view/>
+
+    <footer class="footer">
+      <ul class="footer-nav">
+        <li><a href="#">The Beginning</a></li>
+        <li><a href="#">So far, so good</a></li>
+        <li><a href="#">Mistakes and Advice</a></li>
+      </ul>
+    </footer>
   </div>
-  <!-- <router-view/> -->
 </template>
 
 <script>
@@ -43,34 +49,4 @@
 @import './css/settings.responsive';
 @import './css/grid.font.mixins';
 @import './css/components.content';
-
-  // place the nav at the bottom of the header, on top of the header image
-  .nav {
-  grid-area: 12 / 1 / 12 / span 12;
-  color: $white;
-  }
-  .nav-list {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-    font-size: 1.5rem;
-  }
-
-  // Hover transition:
-  //https://codepen.io/dig-lopes/pen/WLVGda?editors=1100
-  a {
-    color: $white;
-    text-decoration: none;
-    padding: 0 2rem;
-    transition: .5s all ease-in-out;
-  }
-  //ease in and change the text and background color and add a border
-  a:hover {
-    color: $neon-yellow;
-    box-shadow: 0 0 5px $neon-yellow;
-    text-shadow: 0 0 5px $neon-yellow;
-    background-color: rgb(63, 63, 63);
-    border-radius: 3rem;
-    font-size: 2rem;
-  }
 </style>
--- lesson07/assignment07/src/main.js
+++ lesson07/assignment07/src/main.js
@@ -1,44 +1,18 @@
-import { createApp } from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
-import Vue from 'vue'
+
+
+// Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+// Vue.use(IconsPlugin)
+
+import { createApp } from 'vue'
 import App from './App.vue'
-import Header from './views/Header'
-import Home from './views/Home'
-import Images from './views/Images'
-import Accordian from './views/Accordian'
-import Bootgrid from './views/Boot-grid'
 import router from './router'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-// Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
-
-
-// createApp(App).mount('#app')
 createApp(App).use(router).mount('#app')
-createApp(Header).use(router).mount('#header')
-createApp(Home).use(router).mount('#home')
-createApp(Images).use(router).mount('#images')
-createApp(Accordian).use(router).mount('#accordian')
-createApp(Bootgrid).use(router).mount('#boot-grid')
-
-
-// const app = Vue.createApp({
-//     data() {
-//         return {
-//             product: 'Socks',
-//             image: './assets/images/socks_green.jpg',
-//             // solution
-//             url: 'https://www.vuemastery.com/'
-//             // solution
-//         }
-//     }
-// })
-
-
