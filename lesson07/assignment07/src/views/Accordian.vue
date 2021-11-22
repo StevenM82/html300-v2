27750a7ebeb2ca819402010ea45a17337ac4f47a
Gitting better <StevenM82@users.noreply.github.com>
2021-11-22 09:59:38 -0800

    working on accordian now

--- lesson07/assignment07/src/views/Accordian.vue
+++ lesson07/assignment07/src/views/Accordian.vue
@@ -1,81 +1,39 @@
 <template>
   <section class="section">
-    <div id="accordion">
-
-      <!-- .card is the container that will hold all of the information for each individual accordian  -->
-      <div class="card mb-1">
-        <div class="card-header text-center" id="headingOne">
-          <h5 class="mb-0">
-            <!-- The user interacts with the button to make the accordian show/collapse the inner content. The data-target tells the button which element to interact with.  -->
-            <!-- setting data-toggle="collapse" initializes the box with only the button title showing -->
-            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-              Where we started
-            </button>
-          </h5>
-        </div>
-
-        <!-- we attach the button here and assign the accordian its initial state -->
-        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-          <!-- .card-body holds the accordian's content and is shown/hidden when the user selects the corresponding button -->
-          <div class="card-body">
-            <img class="img-thumbnail" src="../assets/the-market.jpg">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      <div id="accordion">
+        <div 
+          class="card mb-1"
+          v-for="box in boxes"
+          :key="box.id">
+            <div class="card-header text-center" @click="box.isCollapsed = !box.isCollapsed">
+              <div>
+                <h5 class="mb-0">{{ box.header }}</h5>
+              </div>
+              <div>
+                <!-- changes the direction of the arrow that is on the box -->
+                <span v-if="box.isCollapsed">&#x2191;</span>
+                <span v-else>&#x2193;</span>
+              </div>
+            </div>
+            <div class="card-body" v-if="box.isCollapsed">
+              <img class="img-thumbnail"> {{ box.image }}
+              {{ box.text }}
+            </div>
           </div>
-        </div>
       </div>
-    </div>
-
-
-    <!-- this makes the card -->
-    <!-- <div class="card">
-      <div class="card-header text-center">
-        <h5 class="mb-0">
-          <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"></button>
-        </h5>
-      </div> -->
-
-    <!-- this makes the content -->
-      <!-- <div id="1">
-        <div class="card-body">
-          {image, text}
-        </div>
-      </div> -->
-
-    <!-- </div> -->
-
-    <div 
-      class="card"
-      v-for="box in boxes"
-      :key="box.id">
-    </div>
-  </section>
-  
+  </section>  
 </template>
 
-  <!-- make the images and their text an object so they can be iterated over
-  use v-if/v-else to make the cards get progressively smaller, but only until a certain point  -->
-  <!-- each card has a card title, and a thumbnail with text in the drop down 
-  
-      <div class="card mx-3 mb-1">
-      <div class="card mx-5 mb-1">
-    -->
-
 <script>
-  export default {
+export default {
     name: 'Accordian',
-    //use v-if/v-else or v-show on the accordian to expand and collapse
     data() {
       return {
         isCollapsed: true,
-        // cardInfo: [
-        //   { image: "../assets/the-market.jpg", text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."},
-        //   { image: "../assets/original-interior.jpg", text: 123, },
-        //   { image: "../assets/tropical-forest.jpg", text: 123, },
-        // ]
         boxes: [
-          {id: 1},
-          {id: 2},
-          {id: 3},
+          {id: 1, header: "Where we started", image: require("../assets/the-market.jpg"), text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."},
+          {id: 2, header: "How it looked", image: require("../assets/original-interior.jpg"), text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."},
+          {id: 3, header: "The quiet", image: require("../assets/tropical-forest.jpg"), text: "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS."},
         ]
       }
     }
