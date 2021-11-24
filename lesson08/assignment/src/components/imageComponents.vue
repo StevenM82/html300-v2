9e4f06dc11303b3547fc503a0cc3184668364491
Gitting better <StevenM82@users.noreply.github.com>
2021-11-23 21:23:19 -0800

    starting work on img component

---
+++ lesson08/assignment/src/components/imageComponents.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    name: 'imageComponent', 
+    props: {
+        src: {
+            type: String,
+            required: true,
+        },
+        alt: {
+            type: String,
+            required: false,
+        },
+        title: {
+            type: String,
+            required: false,
+        }
+    }  
+}
+</script>
+
+<template>
+    <li><img {{image.url}} ></li>
+</template>
+
+
+<style lang="scss">
+
+</style>
